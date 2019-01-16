e5c3a8b48fbf31e6c3c5afad7bc4f6f6cdb82775
Trung Hong Ta <trung.ta@hdwebsoft.co>
2019-01-16 17:29:07 +0700

    .

---
+++ magics.js
@@ -0,0 +1,27 @@
+/**
+ * this a magics file write in js magic
+ * version 1.0
+ * author: trungx
+ */
+
+window.colors = [
+	'danger',
+	'primary',
+	'success',
+	'dark',
+	'light'
+];
+window.members = [
+	{
+		'name':'Son',
+		'birthday':'20/11/2019'
+	},
+	{
+		'name':'Duc',
+		'birthday':'20/11/2019'
+	}
+];
+
+const app = document.querySelector('#leader-board');
+console.log(app);
+
